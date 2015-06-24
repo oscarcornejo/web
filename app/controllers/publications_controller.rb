293e9e60c1fe8344662b997d403d3faa14d98b5b
oscar <oscarcornejo10@gmail.com>
2015-06-24 18:04:23 -0300

    Proyecto arriba

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,5 +1,6 @@
 class PublicationsController < ApplicationController
   before_action :set_publication, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except:[ :index, :show ]
 
   # GET /publications
   # GET /publications.json
--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20150614030154) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
-    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
